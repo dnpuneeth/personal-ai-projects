9c3b194323279d1e5051922257bbbd80502849cb
Puneeth <puneethdh@gmail.com>
2025-08-25 01:09:37 +0530

    Add all missing JavaScript assets to manifest for production compilation

--- apps/redlineai/api/app/assets/config/manifest.js
+++ apps/redlineai/api/app/assets/config/manifest.js
@@ -1,4 +1,8 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../builds
+//= link application.js
+//= link controllers/application.js
+//= link controllers/index.js
+//= link controllers/upload_controller.js
 //= link theme_toggle.js
 //= link profile_picture.js
